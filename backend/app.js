3d119f545d0a25c09217ed96bbc91cfda53680ec
umeshrai01 <ukrai.ukr@gmail.com>
2024-09-20 08:24:12 +0530

    Your commit message here

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,10 @@ const app = express();
 
 const corsOptions = {
   origin: 'https://umeshrai01.github.io',
+<<<<<<< HEAD
+=======
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+>>>>>>> 751cc67 (Your commit message here)
   credentials: true,
 };
 
